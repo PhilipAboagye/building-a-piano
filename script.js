1d2e98394d6f4d11cff6224f0f066ccb51714b1b
Philip <philipab896@gmail.com>
2023-07-16 23:50:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,170 @@
+  // Path to the audio files in the sounds folder()
+  const cKeyAudio = new Audio("sounds/do-c.mp3"); 
+  const dKeyAudio = new Audio("sounds/re-d.mp3");
+  const eKeyAudio = new Audio("sounds/mi-e.mp3");
+  const fKeyAudio = new Audio("sounds/fa-f.mp3");
+  const gKeyAudio = new Audio("sounds/sol-g.mp3");
+  const aKeyAudio = new Audio("sounds/la-a.mp3");
+  const bKeyAudio = new Audio("sounds/si-b.mp3");
+
+  // Path to the audio files in the sounds2 folder()
+  const c2KeyAudio = new Audio("sounds2/do2-c.mp3");
+  const d2KeyAudio = new Audio("sounds2/re2-d.mp3");
+  const e2KeyAudio = new Audio("sounds2/mi2-e.mp3");
+  const f2KeyAudio = new Audio("sounds2/fa2-f.mp3");
+  const g2KeyAudio = new Audio("sounds2/sol2-g.mp3");
+  const a2KeyAudio = new Audio("sounds2/la2-a.mp3");
+  const b2KeyAudio = new Audio("sounds2/si2-b.mp3");
+
+  // Path to the audio files in the sounds3 folder()
+  const c3KeyAudio = new Audio("sounds3/do3-c.mp3");
+  const d3KeyAudio = new Audio("sounds3/re3-d.mp3");
+  const e3KeyAudio = new Audio("sounds3/mi3-e.mp3");
+  const f3KeyAudio = new Audio("sounds3/fa3-f.mp3");
+  const g3KeyAudio = new Audio("sounds3/sol3-g.mp3");
+  const a3KeyAudio = new Audio("sounds3/la3-a.mp3");
+  const b3KeyAudio = new Audio("sounds3/si3-b.mp3");
+
+
+
+ // Assigning element by ID in the div class(first)
+  const cKey = document.getElementById('c-key');
+  const dKey = document.getElementById('d-key');
+  const eKey = document.getElementById('e-key');
+  const fKey = document.getElementById('f-key');
+  const gKey = document.getElementById('g-key');
+  const aKey = document.getElementById('a-key');
+  const bKey = document.getElementById('b-key');
+
+  // Assigning element by ID in the div class(second)
+  const c2Key = document.getElementById('c2-key');
+  const d2Key = document.getElementById('d2-key');
+  const e2Key = document.getElementById('e2-key');
+  const f2Key = document.getElementById('f2-key');
+  const g2Key = document.getElementById('g2-key');
+  const a2Key = document.getElementById('a2-key');
+  const b2Key = document.getElementById('b2-key');
+
+  // Assigning element by ID in the div class(third)
+  const c3Key = document.getElementById('c3-key');
+  const d3Key = document.getElementById('d3-key');
+  const e3Key = document.getElementById('e3-key');
+  const f3Key = document.getElementById('f3-key');
+  const g3Key = document.getElementById('g3-key');
+  const a3Key = document.getElementById('a3-key');
+  const b3Key = document.getElementById('b3-key');
+
+ 
+//==========================================================================
+//First Listener
+//==========================================================================
+  cKey.addEventListener('mousedown', () => {
+      cKeyAudio.currentTime = 1; // Reset the audio to the beginning
+      cKeyAudio.play();
+  });
+
+  dKey.addEventListener('mousedown', () => {
+      dKeyAudio.currentTime = 1; // Reset the audio to the beginning
+      dKeyAudio.play();
+  });
+
+  eKey.addEventListener('mousedown', () => {
+      eKeyAudio.currentTime = 1; // Reset the audio to the beginning
+      eKeyAudio.play();
+  });
+
+  fKey.addEventListener('mousedown', () => {
+      fKeyAudio.currentTime = 1; // Reset the audio to the beginning
+      fKeyAudio.play();
+  });
+
+  gKey.addEventListener('mousedown', () => {
+      gKeyAudio.currentTime = 1; // Reset the audio to the beginning
+      gKeyAudio.play();
+  });
+
+  aKey.addEventListener('mousedown', () => {
+      aKeyAudio.currentTime = 1; // Reset the audio to the beginning
+      aKeyAudio.play();
+  });
+
+  bKey.addEventListener('mousedown', () => {
+      bKeyAudio.currentTime = 1; // Reset the audio to the beginning
+      bKeyAudio.play();
+  });
+//==========================================================================
+
+//Second Listener
+//==========================================================================
+  c2Key.addEventListener('mousedown', () => {
+      c2KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      c2KeyAudio.play();
+  });
+
+  d2Key.addEventListener('mousedown', () => {
+      d2KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      d2KeyAudio.play();
+  });
+
+  e2Key.addEventListener('mousedown', () => {
+      e2KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      e2KeyAudio.play();
+  });
+
+  f2Key.addEventListener('mousedown', () => {
+      f2KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      f2KeyAudio.play();
+  });
+
+  g2Key.addEventListener('mousedown', () => {
+      g2KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      g2KeyAudio.play();
+  });
+
+  a2Key.addEventListener('mousedown', () => {
+      a2KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      a2KeyAudio.play();
+  });
+
+  b2Key.addEventListener('mousedown', () => {
+      b2KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      b2KeyAudio.play();
+  });
+//==========================================================================
+
+//Third Listener
+//==========================================================================
+c3Key.addEventListener('mousedown', () => {
+      c3KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      c3KeyAudio.play();
+  });
+
+  d3Key.addEventListener('mousedown', () => {
+      d3KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      d3KeyAudio.play();
+  });
+
+  e3Key.addEventListener('mousedown', () => {
+      e3KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      e3KeyAudio.play();
+  });
+
+  f3Key.addEventListener('mousedown', () => {
+      f3KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      f3KeyAudio.play();
+  });
+
+  g3Key.addEventListener('mousedown', () => {
+      g3KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      g3KeyAudio.play();
+  });
+
+  a3Key.addEventListener('mousedown', () => {
+      a3KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      a3KeyAudio.play();
+  });
+
+  b3Key.addEventListener('mousedown', () => {
+      b3KeyAudio.currentTime = 1; // Reset the audio to the beginning
+      b3KeyAudio.play();
+  });
